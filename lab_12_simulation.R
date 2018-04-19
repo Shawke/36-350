6678cdd15c07988af6d03ae5a7a5e70bd21e6cab
Shawke <shawke@andrew.cmu.edu>
2018-04-19 11:52:12 -0400

    Update lab_12_simulation.R

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,1 +1,11 @@
-
+generate_data = function(n, p){
+  L = list()
+  covariates = matrix(nrow = n, ncol = p)
+  for(i in 1:p){
+    covariates[,i] = rnorm(p)
+    }
+  responses = rnorm(n)
+  L$covariates = covariates
+  L$responses = responses
+  return(L)
+  }
